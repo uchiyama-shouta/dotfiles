05bdc053f3bdc789f6b555ab1c5598c682e8b300
utiyama <ninjin0604@gmail.com>
2025-08-22 01:49:10 +0900

    fix lsp setteing

--- neovim/nvim/lua/plugins/lsp.lua
+++ neovim/nvim/lua/plugins/lsp.lua
@@ -6,18 +6,8 @@ return {
 
     config = function()
       local caps = require("cmp_nvim_lsp").default_capabilities()
-      -- 依存なし root 判定（deno / node 自動切替）
-      local function root_pattern(patterns)
-        return function(fname)
-          local start = vim.fs.dirname(fname)
-          local found = vim.fs.find(patterns, { path = start, upward = true })[1]
-          return found and vim.fs.dirname(found) or nil
-        end
-      end
-      local deno_root = root_pattern({ "deno.json", "deno.jsonc" })
-      local node_root = root_pattern({ "package.json", "tsconfig.json", "jsconfig.json" })
-
       local lspconfig = require("lspconfig")
+      local on_attach = require("config.lsp_keymaps")
 
       -- Rust
       lspconfig.rust_analyzer.setup({
@@ -34,10 +24,6 @@ return {
       -- TypeScript / JavaScript（Node系）
       lspconfig.ts_ls.setup({
         capabilities = caps,
-        root_dir = function(fname)
-          if deno_root(fname) then return nil end
-          return node_root(fname)
-        end,
         on_attach = function(client, bufnr)
           client.server_capabilities.documentFormattingProvider = false
           client.server_capabilities.documentRangeFormattingProvider = false
@@ -58,12 +44,6 @@ return {
             },
           },
         },
-      })
-
-      -- Deno（deno.json* があると自動ON）
-      lspconfig.denols.setup({
-        root_dir = deno_root,
-        init_options = { lint = true, unstable = true },
       })
 
       -- Web
