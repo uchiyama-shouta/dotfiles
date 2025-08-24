53268356b3ed364b39cbd34731d8f5d3384b3ec5
utiyama <ninjin0604@gmail.com>
2025-08-24 12:49:43 +0900

    setting keymaps

--- neovim/nvim/lua/config/keymaps.lua
+++ neovim/nvim/lua/config/keymaps.lua
@@ -1,4 +1,31 @@
 local map = vim.keymap.set
-map("n", "<leader><space>", "<cmd>Telescope find_files<cr>", { desc = "Find Files" })
-map("n", "<leader>sg",      "<cmd>Telescope live_grep<cr>",  { desc = "Search (ripgrep)" })
-map("n", "<leader>fb",      "<cmd>Telescope buffers<cr>",    { desc = "Buffers" })
+local builtin = require("telescope.builtin")
+
+map("n", "<C-p>", builtin.find_files, { desc = "Find Files (Ctrl-P)" })
+
+map("n", "<leader><space>", builtin.find_files, { desc = "Find Files" })
+map("n", "<leader>ff",      builtin.find_files, { desc = "Files" })
+map("n", "<leader>fr",      builtin.oldfiles,   { desc = "Recent files" })
+map("n", "<leader>fb",      builtin.buffers,    { desc = "Buffers" })
+
+map("n", "<leader>sg",      builtin.live_grep,  { desc = "Search (ripgrep)" })
+map("n", "<leader>sw",      builtin.grep_string,{ desc = "Search word under cursor" })
+map("n", "<leader>sh",      builtin.help_tags,  { desc = "Help" })
+map("n", "<leader>sk",      builtin.keymaps,    { desc = "Keymaps" })
+map("n", "<leader>sc",      builtin.commands,   { desc = "Commands" })
+
+map("n", "<leader>sd",      builtin.diagnostics,        { desc = "Diagnostics (workspace)" })
+map("n", "<leader>ss",      builtin.lsp_document_symbols, { desc = "Document Symbols" })
+map("n", "<leader>sS",      builtin.lsp_workspace_symbols, { desc = "Workspace Symbols" })
+
+map("n", "<leader>f.", function()
+  builtin.find_files({ hidden = true, no_ignore = true })
+end, { desc = "Find Files (hidden, no_ignore)" })
+
+pcall(function()
+  local wk = require("which-key")
+  wk.add({
+    { "<leader>f", group = "file" },
+    { "<leader>s", group = "search" },
+  })
+end)
