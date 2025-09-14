96728a845a2b8b957fad4f6352e83c31a9bd64a3
utiyama <ninjin0604@gmail.com>
2025-09-14 13:22:33 +0000

    add font

--- home.nix
+++ home.nix
@@ -2,6 +2,7 @@
   # 他のカスタム設定モジュールをインポート
   imports = [ ./neovim ./neovim/tools.nix ./shell ./git.nix ./tmux.nix ];
   programs.home-manager.enable = true;
+  fonts.fontconfig.enable = true;
 
   # ユーザー情報の設定
   home = {
@@ -12,6 +13,7 @@
       rust-bin.stable.latest.default
       nodejs_22
       pnpm
+      nerd-fonts.hack
     ];
   };
   nix = {
