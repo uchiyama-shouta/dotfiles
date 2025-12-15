54e27ac9f7e40d6d6db3181049dbc327ec0cee1e
utiyama <ninjin0604@gmail.com>
2025-12-15 23:48:29 +0900

    fix

---
+++ hosts/shota/home-manager.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }: {
+  # 他のカスタム設定モジュールをインポート
+  imports = [ ../../neovim ../../neovim/tools.nix ../../shell ../../git.nix ../../tmux.nix ];
+
+  programs.home-manager.enable = true;
+  fonts.fontconfig.enable = true;
+
+  # ユーザー情報の設定
+  home = {
+    stateVersion = "23.05";
+
+    username = "shota";
+    homeDirectory = "/home/shota";
+
+    packages = with pkgs; [
+      tree
+      rust-bin.stable.latest.default
+      nodejs_22
+      pnpm
+      nerd-fonts.hack
+    ];
+  };
+
+  nix = {
+    enable = true;
+    package = pkgs.nix;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+}
