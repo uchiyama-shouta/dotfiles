7dfbf65f9b39c29bc95d4b825bfb4da7a6e41b15
utiyama <ninjin0604@gmail.com>
2026-05-06 03:40:03 +0900

    add firefox

--- hosts/shota/home-manager.nix
+++ hosts/shota/home-manager.nix
@@ -3,6 +3,25 @@
   imports = [ ../../neovim ../../neovim/tools.nix ../../shell ../../git.nix ../../tmux.nix ];
 
   programs.home-manager.enable = true;
+
+  programs.firefox = {
+    enable = true;
+
+    # profiles.default = {
+    #   isDefault = true;
+    #
+    #   settings = {
+    #     "browser.startup.homepage" = "about:blank";
+    #     "privacy.trackingprotection.enabled" = true;
+    #   };
+    #
+    #   extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+    #     ublock-origin
+    #     bitwarden
+    #   ];
+    # };
+  };
+
   fonts.fontconfig.enable = true;
 
   # ユーザー情報の設定
@@ -18,8 +37,9 @@
       nodejs_22
       pnpm
       nerd-fonts.hack
+      codex
     ];
-  };
+ };
 
   nix = {
     enable = true;
